515ae1029c75a764eb11cf7aaf7a117fb84fa550
Daniel <daniel@pink-taco.local>
2021-07-08 19:03:25 -0400

    figured out the valid key

---
+++ objects/exercise2.js
@@ -0,0 +1,10 @@
+// Which of the following values are valid keys for an object?
+
+// 1 valid key
+// '1' valid key
+// undefined //valid key
+// 'hello world' valid key
+// true valid key
+// 'true' valid key
+
+

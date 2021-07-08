87d7e911f704288e16354ebb1ec53338c3784a89
Daniel <daniel@pink-taco.local>
2021-07-08 16:07:41 -0400

    completed nested for-loop to log even values

---
+++ arrays/exercise3.js
@@ -0,0 +1,17 @@
+//Let's make the problem a little harder. In this problem, we're again interested in even numbers, but this time the numbers are in nested arrays in a single outer array.
+
+let myArray = [
+  [1, 3, 6, 11],
+  [4, 2, 4],
+  [9, 17, 16, 0],
+];
+
+//nested for-loop for a nested array 
+for (let i = 0; i < myArray.length; i++) {
+  for(let j = 0; j < myArray[i].length; j++){
+    let value = myArray[i][j];
+    if(value % 2 === 0){
+      console.log(value)
+    }
+  }
+}

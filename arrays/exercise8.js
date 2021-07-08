601b354868bb10c1e7bafd994e39c890f8fb8898
Daniel <daniel@pink-taco.local>
2021-07-08 18:20:43 -0400

    solved oddlengths with the reduce function

---
+++ arrays/exercise8.js
@@ -0,0 +1,22 @@
+// Write a function similar to the oddLengths function from Exercise 6, but don't use map or filter. Instead, try to use the reduce method.
+
+let arr = ['a', 'abcd', 'abcde', 'abc', 'ab'];
+
+
+function oddLengths (arr) {
+  return arr.reduce(function(accumulator, currentValue){
+    let numberLength = currentValue.length;
+    if(numberLength % 2 === 1){
+      accumulator.push(numberLength)
+    }
+    return accumulator;
+  }, [])
+}
+
+
+
+
+
+
+
+console.log(oddLengths(arr)); // => [1, 5, 3]

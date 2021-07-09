f3f6fc95456e203f8e13f69c29d1c52ff9901836
Daniel <daniel@pink-taco.local>
2021-07-09 09:42:34 -0400

    primitive value review

---
+++ objects/exercise6.js
@@ -0,0 +1,10 @@
+// Which of the following values are primitive values? Which are objects? Which are neither?
+
+// "foo" => string, primitive value
+// 3.1415 => number, primitive value
+// [ 'a', 'b', 'c' ] => array
+// false => boolean, primitive value
+// foo => not a primitive value, just an identifier
+// function bar() { return "bar"; } => function, not primitive value
+// undefined => undefined => primitive value
+// { a: 1, b: 2 } => object => not primitive value

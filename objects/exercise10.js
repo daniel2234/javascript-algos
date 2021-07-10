98cc08b493108ef084cc475a09912c8414eb8bdf
Daniel <daniel@pink-taco.local>
2021-07-10 11:27:06 -0400

    solved of objects and primitive values

---
+++ objects/exercise10.js
@@ -0,0 +1,10 @@
+//How many primitive values are there in the following expression? Identify them. How many objects are there in the expression? Identify those objects.
+
+
+[1, 2, ["a", ["b", false]], null, {}]
+
+//4 objects
+//6 primitive values
+//typeof null states it is a object, so maybe 5 objects
+//javascript docs state that the null is primitive value
+

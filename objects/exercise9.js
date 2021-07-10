97cec3256f44c1c99fabbd66adaf53d6cad2dd3d
Daniel <daniel@pink-taco.local>
2021-07-10 11:26:33 -0400

    figured out function calls

--- objects/exercise9.js
+++ objects/exercise9.js
@@ -8,7 +8,7 @@ let foo = {
 let qux = 'hello'; //declare and assign a variable with a string primitive value of 'hello'
 
 function bar(argument1, argument2) {
-  argument1.a = 'hi';
+  argument1.a = 'hi'; //this argument 
   argument2 = 'hi';
 } //using a reserved word of function and naming a function of bar with associated parameters of argument1 and argument2 
 
@@ -16,3 +16,6 @@ bar(foo, qux);
 
 console.log(foo.a);
 console.log(qux);
+
+//this will log hi and hello, 
+//objects are mutable and primitive are immutable
